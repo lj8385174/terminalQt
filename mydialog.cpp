542360bf60e23b297286505f5c0d78bc1e53a47d
Ronald Reichmuth <ronald.reichmuth@steinel.ch>
2017-07-06 11:55:33 +0200

    reduce amount of data in the stream, add ledset analysis

--- mydialog.cpp
+++ mydialog.cpp
@@ -46,31 +46,83 @@ void MyDialog::updateValue()
 
 
     //read 'Sachnummer'
-    quint8 s3,s2,s1,s0;
-    s3 = dataStream.at(3);
-    s2 = dataStream.at(4);
-    s1 = dataStream.at(5);
-    s0 = dataStream.at(6);
-    quint32 sum = s3*16777216 + s2*65536 + s1*256 + s0;
-    quint32 Sachnummer = sum;
+    //quint8 s3,s2,s1,s0;
+    //s3 = dataStream.at(3);
+    //s2 = dataStream.at(4);
+    //s1 = dataStream.at(5);
+    //s0 = dataStream.at(6);
+    //quint32 sum = s3*16777216 + s2*65536 + s1*256 + s0;
+    //quint32 Sachnummer = sum;
 
     //set ui labels
     ui->plainTextEdit->setPlainText(MyDialog::dataStream.toHex('-').toUpper());
 
     ui->label_bytes->setText(tr("Bytes: %1").arg(QString::number(dataStream.at(2))));
-    ui->label_version->setText(tr("Version: %1").arg(QString::number(dataStream.at(7))));
-    ui->label_sachnummer->setText(tr("Sachnummer: %1").arg(QString::number(Sachnummer)));
+    //ui->label_version->setText(tr("Version: %1").arg(QString::number(dataStream.at(7))));
+    //ui->label_sachnummer->setText(tr("Sachnummer: %1").arg(QString::number(Sachnummer)));
 
     QByteArray temp = dataStream;
     QString gugus = dataStream.toHex().toUpper();
 
-    quint8 B19 = dataStream.at(19);
-    quint8 B20 = dataStream.at(20);
-    quint8 B21 = dataStream.at(21);
+    quint8 B3 = dataStream.at(3);
+    quint8 B4 = dataStream.at(4);
+    quint8 B5 = dataStream.at(5);
 
-    ui->label_byte1->setText("Byte1: " + QString::number(B19));
-    ui->label_byte2->setText("Byte2: " + QString::number(B20));
-    ui->label_byte3->setText("Byte3: " + QString::number(B21));
+
+    quint8 CommandByte  = B3;
+    quint8 DataByteHigh = B4;
+    quint8 DataByteLow  = B5;
+
+    ui->label_byte1->setText("CommandByte : " + QString::number(CommandByte));
+    ui->label_byte2->setText("DataByteHigh: " + QString::number(DataByteHigh));
+    ui->label_byte3->setText("DataByteLow : " + QString::number(DataByteLow));
+
+    static QString EventList = "";
+
+    if (CommandByte == 0x00)
+    {
+        quint16 LightMeasurement = (quint16)DataByteHigh;
+        LightMeasurement = (LightMeasurement << 8);
+        LightMeasurement = (LightMeasurement | (quint16)DataByteLow);
+        LightMeasurement = (LightMeasurement & 0xFFFE)>>1;
+        quint8 Nachkommastellen = (quint8)(LightMeasurement & 0x00F);
+        LightMeasurement = (LightMeasurement & 0x7FF0)>>4;
+        ui->LightMeasurement->setText(QString::number(LightMeasurement)+" lx");
+
+        float Value = LightMeasurement;
+        if (Nachkommastellen & 0x01) Value = Value + 0.0625;
+        if (Nachkommastellen & 0x02) Value = Value + 0.1250;
+        if (Nachkommastellen & 0x04) Value = Value + 0.2500;
+        if (Nachkommastellen & 0x08) Value = Value + 0.5000;
+        alsData->append((qreal)i,(qreal)Value);
+
+        ui->LightMeasurement->setText(QString::number(Value,'f',4)+" lx");
+
+        quint8 Motion =  (DataByteLow & 0x01);
+        if (Motion == 0x01)
+        {
+            ui->Motion->setText("Motion");
+        }
+        else
+        {
+            ui->Motion->setText("-");
+        }
+    }
+    else if (CommandByte == 0x01)
+    {
+        QString Event;
+        if (DataByteLow == 0x00) Event = "Short Push";
+        if (DataByteLow == 0x01) Event = "Double Click";
+        if (DataByteLow == 0x02) Event = "Longpush";
+        if (DataByteLow == 0x03) Event = "Ultra Longpush";
+        ui->Motion->setText(Event);
+
+        if (EventList.size() > 100) EventList = "";
+
+        EventList.append(Event + "\n");
+        ui->EventList->setPlainText(EventList);
+
+    }
 }
 
 
@@ -100,7 +152,7 @@ void MyDialog::dataBuffer(QByteArray buf)
 
             if(buffer.size()>=(start+lengthOfStream))
             {
-                tmp = buffer.mid(start,lengthOfStream+1);
+                tmp = buffer.mid(start,lengthOfStream);
 
                 MyDialog::dataStream = tmp;
                 ui->plainTextEdit->setPlainText(tmp.toHex().toUpper());
@@ -109,11 +161,11 @@ void MyDialog::dataBuffer(QByteArray buf)
                 //TODO: Plausiblitätscheck
 
                 //add data to q line series data
-                quint8 highByte, lowByte;
-                highByte = dataStream.at(12);
-                lowByte = dataStream.at(13);
-                quint32 sum = highByte*256 + lowByte;
-                alsData->append((qreal)i,(qreal)sum);
+                //quint8 highByte, lowByte;
+                //highByte = dataStream.at(12);
+                //lowByte = dataStream.at(13);
+                //quint32 sum = highByte*256 + lowByte;
+                //alsData->append((qreal)i,(qreal)sum);
 
 
                 tmp.clear();
@@ -128,7 +180,7 @@ void MyDialog::dataBuffer(QByteArray buf)
 void MyDialog::on_pushButton_clicked()
 {
     //createRandomData();
-
+    createGraph();
     mGraphWindow->show();
 }
 
